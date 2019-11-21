56b129e68d67c4ef2b0934a2b2ffab5c66ef8dbb
Mark <mark.balazon@gmail.com>
2019-11-20 20:32:07 -0500

    created albums.html

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('startbootstrap-grayscale-gh-pages/index.html', title='SASE')
+
+
+@app.route('/albums')
+def albums():
+    return render_template('startbootstrap-grayscale-gh-pages/albums.html', title='SASE')
