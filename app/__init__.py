25b0469406a450972a438b140775144980ef6cd3
Mark <mark.balazon@gmail.com>
2020-03-10 15:06:13 -0400

    touched up eboard

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.debug = True
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = 1
+app.config['MAIL_USERNAME'] = 'kungfucut1@gmail.com'
+app.config['MAIL_PASSWORD'] = 'fufufux3'
+
+mail = Mail(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+from app.email import send_email
+from flask_mail import Mail, Message
+from app import mail
 
 
 @app.route('/')
@@ -11,3 +14,10 @@ def index():
 @app.route('/albums')
 def albums():
     return render_template('startbootstrap-grayscale-gh-pages/albums.html', title='SASE')
+
+
+@app.route('/email', methods=['GET', 'POST'])
+def email():
+    text = request.form['inem']
+    send_email('Add me to the list serv please', 'kungfucut1@gmail.com', ['ubsase@gmail.com'], 'body', '<p>' + text + '</p>')
+    return render_template('startbootstrap-grayscale-gh-pages/index.html', title='SASE')
