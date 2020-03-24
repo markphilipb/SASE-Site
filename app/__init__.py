56aa386af0348d57e7d8605d831a533513cc1ef0
Mark <mark.balazon@gmail.com>
2020-03-24 18:45:32 -0400

    edited __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = 1
-app.config['MAIL_USERNAME'] = 'kungfucut1@gmail.com'
-app.config['MAIL_PASSWORD'] = 'fufufux3'
+
 
 mail = Mail(app)
 
